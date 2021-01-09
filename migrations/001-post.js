33d4c89173f1fa18b389551058e08ed92adbb204
Said Akhmedbayev <said.akh@gmail.com>
2021-01-09 15:49:38 +0600

    initialized db

---
+++ migrations/001-post.js
@@ -0,0 +1,24 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("post", function (table) {
+      table.increments("id");
+      table.string("title", 255).notNullable();
+      table.string("text").notNullable();
+    })
+    .createTable("comment", function (table) {
+      table.increments("id");
+      table
+        .integer("postId")
+        .unsigned()
+        .references("id")
+        .inTable("post")
+        .useNullAsDefault()
+        .onDelete("CASCADE");
+      table.string("author", 255).notNullable();
+      table.string("text").notNullable();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("post");
+};
