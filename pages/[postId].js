1fcfeec0fe61d96fcad5b0489d254005939fc174
Said Akhmedbayev <said.akh@gmail.com>
2021-01-09 20:37:36 +0600

    added read post details data

--- pages/[postId].js
+++ pages/[postId].js
@@ -1,47 +1,91 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import { blogPosts } from ".";
+import { useQuery } from "react-query";
+// import { blogPosts } from ".";
 import AddNewComment from "../components/AddNewComment";
 import Layout from "../components/common/Layout";
 import styles from "../styles/Post.module.css";
+
+import { request, gql } from "graphql-request";
+
+const getPostById = async ({ queryKey }) => {
+  const [_key, id] = queryKey;
+  const query = gql`
+    query GetPostById($id: ID!) {
+      getPostById(id: $id) {
+        id
+        title
+        text
+        comments {
+          id
+          postId
+          author
+          text
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    id,
+  };
+
+  const data = await request(
+    "http://localhost:3000/api/graphql",
+    query,
+    variables
+  );
+
+  return data.getPostById;
+};
 
 export default function PostDetails() {
   const {
     query: { postId },
   } = useRouter();
 
-  const currentPost = blogPosts.find((post) => post.id === parseInt(postId));
+  console.log("FFF postId", postId);
 
-  if (!currentPost) return <div>Loading...</div>;
+  const { data: currentPost, isLoading } = useQuery(
+    ["getPostById", postId],
+    getPostById,
+    {
+      enabled: !!postId,
+    }
+  );
+
+  if (isLoading || !currentPost) return <div>Loading...</div>;
 
   return (
     <Layout>
-      <h3>
-        {currentPost.title}{" "}
-        <Link href={`/edit/post/${currentPost.id}`}>
-          <a style={{ fontWeight: "normal", color: "blue" }}>Edit</a>
-        </Link>
-      </h3>
+      <div>
+        <h3>
+          {currentPost.title}{" "}
+          <Link href={`/edit/post/${currentPost.id}`}>
+            <a style={{ fontWeight: "normal", color: "blue" }}>Edit</a>
+          </Link>
+        </h3>
 
-      <p>{currentPost.text}</p>
-      <div style={{ width: "100%" }}>
-        <AddNewComment post={currentPost} />
-      </div>
-      <div style={{ width: "100%" }}>
-        {currentPost.comments.map((comment) => (
-          <div key={comment.id} className={styles["card-comment"]}>
-            <div style={{ marginBottom: "1rem" }}>{comment.text}</div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "flex-end",
-              }}
-            >
-              ~ {comment.author}
+        <p>{currentPost.text}</p>
+        <div style={{ width: "100%" }}>
+          <AddNewComment post={currentPost} />
+        </div>
+        <div style={{ width: "100%" }}>
+          {currentPost.comments.map((comment) => (
+            <div key={comment.id} className={styles["card-comment"]}>
+              <div style={{ marginBottom: "1rem" }}>{comment.text}</div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                ~ {comment.author}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </Layout>
   );
